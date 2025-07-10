6ba58cbeb57cfc1c64b28c4826d509bd871e617a
Gaelle Tohme <122909212+Gaelle04@users.noreply.github.com>
2025-07-10 18:00:53 +0300

    Created the footer UI

--- aura-app/src/app/core/layout/footer/footer.ts
+++ aura-app/src/app/core/layout/footer/footer.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, CommonModule],
   templateUrl: './footer.html',
   styleUrl: './footer.scss'
 })
